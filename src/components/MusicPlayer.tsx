03e4996a584814791d573e7b288166be11247f72
HieuNT.Vogue <hieudeptrai196@gmail.com>
2026-04-20 21:20:16 +0700

    up

--- src/components/MusicPlayer.tsx
+++ src/components/MusicPlayer.tsx
@@ -1,4 +1,4 @@
-import { useState, useRef, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { Music, Music2 } from 'lucide-react';
 import { motion, AnimatePresence } from 'framer-motion';
 
